314a1a66a57416212ce1411029dc22f6c6115eeb
Ayush Kumar <51288637+ayushkr7@users.noreply.github.com>
2021-09-16 12:54:19 +0530

    functionalities added

--- app.js
+++ app.js
@@ -13,3 +13,31 @@ var colors = [
   '#73A857'
 ];
 
+const text = document.getElementById('text'), qauthor = document.getElementById('quote-author'), qbtn = document.getElementById('new-quote');
+
+var currentQuote,currentAuthor;
+function randomQuotes(){
+  fetch('https://api.quotable.io/random')
+  .then(response => response.json())
+  .then(data => {
+  text.textContent = data.content;
+  qauthor.textContent = data.author;
+  currentQuote = data.content;
+  currentAuthor = data.author;
+  document.getElementById("tweet-quote").href = 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor);
+  });
+}
+
+randomQuotes();
+
+function randColor(){
+  let r = Math.floor(Math.random() * 12);
+  
+  document.body.style.background = colors[r];
+  document.getElementById("text").style.color = colors[r];
+  document.getElementById("author").style.color = colors[r];
+  document.getElementById("new-quote").style.background = colors[r]; document.getElementById("twitter").style.background = colors[r];
+}
+
+randColor();
+
